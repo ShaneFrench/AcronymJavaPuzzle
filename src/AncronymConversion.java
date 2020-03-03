de5973569be1089ef8b65603ba863684a5fce494
shane.french <frenchywow@gmail.com>
2020-03-03 11:15:38 +0000

    Array Commit

--- src/AncronymConversion.java
+++ src/AncronymConversion.java
@@ -1,7 +1,15 @@
 public class AncronymConversion {
 
     public static void main(String[] args) {
-        String[] arr = {}
+        String[] arr = {"Portable Network Graphics",
+                "Ruby on Rails",
+                "HyperText Markup Language",
+                "First In, First Out",
+                "PHP: Hypertext Preprocessor",
+                "GNU Image Manipulation Program",
+                "Complementary metal-oxide semiconductor",
+                " Cat in a Hat"};
+
 
     }
 }
